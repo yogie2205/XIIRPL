1dd1054ab387b1ae75b561dfb3dd463c03042d34
Wahyu Wiyoko <wahyuwyko@gmail.com>
2021-08-24 22:00:22 +0800

    Meng-update project BangunForm

--- Java Interface/BangunForm/src/MainWindow.java
+++ Java Interface/BangunForm/src/MainWindow.java
@@ -14,7 +14,18 @@ public class MainWindow extends javax.swing.JFrame {
      * Creates new form MainWindow
      */
     public MainWindow() {
+        String lookAndFeel = javax.swing.UIManager.getCrossPlatformLookAndFeelClassName();
+        try {
+            javax.swing.UIManager.setLookAndFeel(lookAndFeel);
+        } catch(Exception e) {
+            
+        }
         initComponents();
+        this.setTitle("Bangun Datar dan Bangun Ruang");
+        this.setLocationRelativeTo(this);
+        this.setResizable(false);
+        
+        
     }
 
     /**
@@ -35,6 +46,7 @@ public class MainWindow extends javax.swing.JFrame {
         btnHitung1 = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
         jPanel2 = new javax.swing.JPanel();
         cmbBangunRuang = new javax.swing.JComboBox<>();
         txtPertama2 = new javax.swing.JTextField();
@@ -50,12 +62,19 @@ public class MainWindow extends javax.swing.JFrame {
         jPanel1.setBorder(javax.swing.BorderFactory.createTitledBorder("Bangun Datar"));
 
         cmbbangunDatar.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Persegi", "Lingkaran", "Segitiga" }));
+        cmbbangunDatar.addItemListener(new java.awt.event.ItemListener() {
+            public void itemStateChanged(java.awt.event.ItemEvent evt) {
+                cmbbangunDatarItemStateChanged(evt);
+            }
+        });
 
         btnHitung1.setText("Hitung");
 
         jLabel1.setText("Luas");
 
         jLabel2.setText("Keliling");
+
+        jLabel5.setText("Wahyu Alvi Wiyoko");
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -68,22 +87,26 @@ public class MainWindow extends javax.swing.JFrame {
                     .addComponent(txtPertama1))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jLabel2)
-                    .addComponent(jLabel1)
-                    .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                        .addComponent(txtLuas1, javax.swing.GroupLayout.DEFAULT_SIZE, 90, Short.MAX_VALUE)
-                        .addComponent(txtKeliling)))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addComponent(jLabel2)
+                        .addComponent(jLabel1)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                            .addComponent(txtLuas1, javax.swing.GroupLayout.DEFAULT_SIZE, 90, Short.MAX_VALUE)
+                            .addComponent(txtKeliling)))
+                    .addComponent(jLabel5, javax.swing.GroupLayout.Alignment.TRAILING))
                 .addContainerGap())
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGap(134, 134, 134)
                 .addComponent(btnHitung1)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap(156, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(cmbbangunDatar, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(cmbbangunDatar, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel5))
                 .addGap(16, 16, 16)
                 .addComponent(jLabel1)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
@@ -104,6 +127,11 @@ public class MainWindow extends javax.swing.JFrame {
         jPanel2.setBorder(javax.swing.BorderFactory.createTitledBorder("Bangun Ruang"));
 
         cmbBangunRuang.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Bola", "Kubus", "Tabung" }));
+        cmbBangunRuang.addItemListener(new java.awt.event.ItemListener() {
+            public void itemStateChanged(java.awt.event.ItemEvent evt) {
+                cmbBangunRuangItemStateChanged(evt);
+            }
+        });
 
         btnHitung2.setText("Hitung");
 
@@ -177,6 +205,22 @@ public class MainWindow extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void cmbbangunDatarItemStateChanged(java.awt.event.ItemEvent evt) {//GEN-FIRST:event_cmbbangunDatarItemStateChanged
+        if (cmbbangunDatar.getSelectedItem().toString().equals("Lingkaran")) {
+            txtKedua1.setVisible(false);
+        } else {
+            txtKedua1.setVisible(true);
+        }
+    }//GEN-LAST:event_cmbbangunDatarItemStateChanged
+
+    private void cmbBangunRuangItemStateChanged(java.awt.event.ItemEvent evt) {//GEN-FIRST:event_cmbBangunRuangItemStateChanged
+        if (cmbBangunRuang.getSelectedItem().toString().equals("Tabung")) {
+            txtKedua2.setVisible(true);
+        } else {
+            txtKedua2.setVisible(false);
+        }
+    }//GEN-LAST:event_cmbBangunRuangItemStateChanged
+
     /**
      * @param args the command line arguments
      */
@@ -221,6 +265,7 @@ public class MainWindow extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JTextField txtKedua1;
